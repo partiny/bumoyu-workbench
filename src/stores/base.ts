04a8986a4a910e2fe1f0c54e85b7dcfec73a83d4
partiny <partiny@163.com>
2025-07-14 16:33:23 +0800

    feat: 取消iframe的判断

--- src/stores/base.ts
+++ src/stores/base.ts
@@ -2,7 +2,7 @@ import { computed } from "vue"
 
 const useBaseInfo = () => {
   // 判断当前页面是否在iframe中
-  const isInIframe = computed(() => self !== top)
+  const isInIframe = computed(() => self !== top && false)
 
   return {
     /**判断当前页面是否在iframe中 */
